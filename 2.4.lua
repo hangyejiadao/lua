1b50fad92f01e64825eac317c0750846af62bdce
hangyejiadao <953917332@qq.com>
2017-12-24 22:50:07 +0800

    lua

---
+++ 2.4.lua
@@ -0,0 +1,14 @@
+print("10"+1)
+print("10+1")
+print("-5.3e-10"*"2")
+-- print("hello"+1)
+-- print(10 ..20)
+-- line=io.read()
+-- n=tonumber(line)
+-- if n==nil then
+--      error(line .."is not a valid number")
+-- else 
+--     print(n*2)
+-- end
+-- print(tostring(10)=="10")
+-- print(10 ..""=="10")
